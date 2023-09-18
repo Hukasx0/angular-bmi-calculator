99fe5abf3279a2f80b362dac13faa2d821410c06
Hukasx0 <hubertkasp13@gmail.com>
2023-09-18 20:06:32 +0200

    Initial commit on github pages

--- src/app/calculator/types/bmi_type.ts
+++
@@ -1,9 +0,0 @@
-export enum BmiType {
-    Err = 'Niepoprawna wartość',
-    Niedowaga = 'Niedowaga',
-    Poprawne = 'Poprawne',
-    Nadwaga = 'Nadwaga',
-    Otylosc = 'Otyłość',
-    Otylosc2 = 'Otyłość stopnia drugiego',
-  }
-  
