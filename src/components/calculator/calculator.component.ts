91d6bbc762c7a4323c86db02951f13dfb5f569f1
Hukasx0 <82332291+Hukasx0@users.noreply.github.com>
2023-09-18 17:37:09 +0200

    Wrote proper BMI calculator

--- src/components/calculator/calculator.component.ts
+++ src/components/calculator/calculator.component.ts
@@ -12,16 +12,37 @@ import { BmiType } from '../../interfaces/bmi_type';
 export class Calculator {
   waga: number = 0;
   wzrost: number = 0;
+  pokaz: boolean = false;
+
   get bmi(): number {
     return parseFloat((this.waga / (this.wzrost ** 2 / 10000)).toFixed(2));
   }
-  oblicz_bmi(): BmiType {
-    if (this.bmi < 18.5) {
-      return BmiType.Niedowaga;
-    } else if (this.bmi >= 18.5 && this.bmi < 24.9) {
-      return BmiType.Poprawne;
-    } else {
-      return BmiType.Nadwaga;
-    }
+  get poprawnosc_wagi(): boolean {
+    return typeof this.waga === 'number' && this.waga <= 0;
+  }
+  get poprawnosc_wzrostu(): boolean {
+    return typeof this.wzrost === 'number' && this.wzrost <= 0;
+  }
+  get poprawny_typ_bmi(): boolean {
+    return (
+      this.kategoria_bmi() !== BmiType.Err &&
+      this.bmi < Number.POSITIVE_INFINITY &&
+      this.bmi >= 0
+    );
+  }
+  pokaz_wartosci() {
+    this.pokaz = !this.pokaz;
+  }
+  kategoria_bmi(): BmiType {
+    const bmi_type: Array<{ type: BmiType; max: number }> = [
+      { type: BmiType.Niedowaga, max: 18.4 },
+      { type: BmiType.Poprawne, max: 24.9 },
+      { type: BmiType.Nadwaga, max: 29.9 },
+      { type: BmiType.Otylosc, max: 34.9 },
+      { type: BmiType.Otylosc2, max: Number.POSITIVE_INFINITY },
+    ];
+    return (
+      bmi_type.find((item) => this.bmi <= item.max) || { type: BmiType.Err }
+    ).type;
   }
 }
--- src/interfaces/bmi_type.ts
+++ src/interfaces/bmi_type.ts
@@ -1,5 +1,8 @@
 export enum BmiType {
+  Err = 'Niepoprawna wartość',
   Niedowaga = 'Niedowaga',
   Poprawne = 'Poprawne',
   Nadwaga = 'Nadwaga',
+  Otylosc = 'Otyłość',
+  Otylosc2 = 'Otyłość stopnia drugiego',
 }
